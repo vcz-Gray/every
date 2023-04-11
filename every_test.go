f6630176045445a2f772f52768e25400b5b856aa
QuePark <gyuhapark11@gmail.com>
2023-04-11 15:16:37 +0900

    Update examples

--- every_test.go
+++ every_test.go
@@ -1,12 +1,57 @@
 package every
 
 import (
+	"fmt"
 	"math"
 	"math/cmplx"
+	strs "strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func ExampleInts() {
+	fmt.Println(Ints([]int{-5, -3, -1, 1, 3, 5}, func(n int) bool {
+		return int(math.Abs(float64(n)))%2 == 1
+	}))
+	fmt.Println(Ints([]int{-4, -2, 0, 2, 4}, func(n int) bool {
+		return int(math.Abs(float64(n)))%2 == 0
+	}))
+}
+
+func ExampleFloats() {
+	fmt.Println(Floats([]float64{10.1, 10.2, 10.3, 10.4, 10.5}, func(n float64) bool {
+		return math.Ceil(n) == float64(11)
+	}))
+}
+
+func ExampleStrings() {
+	fmt.Println(Strings([]string{"An apple", "A banana", "A guaba"}, func(s string) bool {
+		return strs.HasPrefix(s, "An")
+	}))
+
+	fmt.Println(Strings([]string{"+82-10-0000-0000", "+82-10-4654-0000"}, func(s string) bool {
+		return strs.HasPrefix(s, "+82-10")
+	}))
+}
+
+func ExampleBytes() {
+	fmt.Println(Bytes([]byte{'A', 'B', 'C', 'D'}, func(b byte) bool {
+		return b < 'a'
+	}))
+}
+
+func ExampleBools() {
+	fmt.Println(Bools([]bool{true, true, true}, func(b bool) bool {
+		return b == true
+	}))
+}
+
+func ExampleComplexes() {
+	fmt.Println(Complexes([]complex128{cmplx.Inf()}, func(c complex128) bool {
+		return cmplx.IsInf(c)
+	}))
+}
 
 func TestInts(t *testing.T) {
 	is := assert.New(t)
